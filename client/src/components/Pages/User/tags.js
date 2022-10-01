db915db46c44fa80790fd3f9b13e0a44bbd70a05
RobertDragoi <robertdragoi20@gmail.com>
2022-10-01 16:36:04 +0300

    Refactor user page and add tags for placeholders

---
+++ client/src/components/Pages/User/tags.js
@@ -0,0 +1,17 @@
+const userTags = {
+  save: 'Salvează',
+  edit: 'Editează',
+  exit: 'Inchide',
+  notSet: 'Nesetat',
+  email: 'Email',
+  fullName: 'Nume complet',
+  companyName: 'Nume companie',
+  address: 'Adresă',
+  education: 'Studii',
+  activity: 'Domeniu de activitate',
+  birthDate: 'Dată de naștere',
+  creationDate: 'Data înfințării',
+  description: 'Descriere',
+  posts: 'Anunțuri',
+};
+export { userTags };
