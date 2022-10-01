efcdf0c0c59ebee2d86bcf14f80ce47d14dccca3
RobertDragoi <robertdragoi20@gmail.com>
2022-10-01 19:10:25 +0300

    Add tags for welcome page

---
+++ client/src/components/Pages/Welcome/tags.js
@@ -0,0 +1,8 @@
+const welcomeTags = {
+  title1: 'Bun venit pe',
+  title2: 'Student connect',
+  subTitle: 'O platformă pentru studenți și compani',
+  homeButton: 'Vezi anunțurile',
+  join: 'Înscriete',
+};
+export { welcomeTags };
