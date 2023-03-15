1f4f67ecc0611b8f64e6bece078ac15e64a9a354
RobertDragoi <robertdragoi20@gmail.com>
2023-03-15 21:36:53 +0200

    Dockerize the client

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,13 +3,9 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  optimizeDeps: {
-    include: ['linked-dep'],
-  },
-  build: {
-    commonjsOptions: {
-      include: [/linked-dep/, /node_modules/],
-    },
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
   },
   plugins: [
     react({
