c10f1da4ddf7e5c676a5a0b74d00f4b44fe4af76
RobertDragoi <robertdragoi20@gmail.com>
2024-05-18 17:31:51 +0300

    Increase limit

--- server/packages/post-server/main.js
+++ server/packages/post-server/main.js
@@ -15,7 +15,8 @@ mongoose
     useCreateIndex: true,
   })
   .then(() => logger.info('Connected to MongoDB from post-server'));
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb' }));
 app.use('/public', express.static('public'));
 app.use('/api/post', require('./routes'));
 app.listen(config.PORT, () => {
